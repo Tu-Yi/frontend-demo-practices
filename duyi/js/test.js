60a0cd728daf024e383500843d7c54f870b08319
kun.yuan <yk_niliv@qq.com>
2021-09-14 13:14:16 +0800

    add all

--- duyi/js/test.js
+++ duyi/js/test.js
@@ -1,1 +1,16 @@
-function Deng(name, wife) {}
+var a = 'opkhijpokhiajpokijhpokipohjkpojhkpohjkphbjpokopkijhkijhpo'
+
+function findSingleChar(str) {
+    var len = str.length
+    for (var i = 0; i < len; i++) {
+        var firStr = str.slice(i + 1)
+        var secStr = str.slice(0, i)
+        if (firStr.indexOf(str[i]) == -1 && secStr.indexOf(str[i]) == -1) {
+            return str[i]
+        } else {
+            continue
+        }
+    }
+}
+
+console.log(findSingleChar(a)) //a
