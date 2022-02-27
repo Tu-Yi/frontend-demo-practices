893b06872a375c974f882b81fbed59e2c0e84778
kun.yuan <yk_niliv@qq.com>
2022-02-27 21:12:36 +0800

    a

---
+++ practice_css/duyi/TopNav/index.js
@@ -0,0 +1,10 @@
+/**添加导航菜单事件 */
+let Tags = document.getElementsByClassName('menuTrigger')
+for (let i = 0; i < Tags.length; i++) {
+    Tags[i].onmouseenter = function(e) {
+        e.currentTarget.classList.add('active')
+    }
+    Tags[i].onmouseleave = function(e) {
+        e.currentTarget.classList.remove('active')
+    }
+}
